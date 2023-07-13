5b8afaa8d4d1c2ef21e89f2046f81a89be60421a
Osman <114867365+osmansam@users.noreply.github.com>
2023-07-12 21:41:04 -0400

    tailwindcss and redux packages are added

---
+++ src/shared/types.ts
@@ -0,0 +1,9 @@
+export enum SelectedPage {
+  Home = "home",
+  AboutMe = "aboutme",
+  WhatIDo = "whatido",
+  Resume = "resume",
+  Portfolio = "portfolio",
+  Testimonials = "testimonials",
+  Contact = "contact",
+}
